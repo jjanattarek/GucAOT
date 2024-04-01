7acbf72ba3dd55aff1f0dfbc78ebce77a33e7bc2
Ahmed Mohamed Khadrawy <ahmed.khadrawy2004@gmail.com>
2024-04-01 12:34:44 +0200

    update

--- src/module-info.java
+++ src/module-info.java
@@ -4,12 +4,8 @@
 /**
  * 
  */
-module AttackOnTitans {
+module AttackOnTitans
+{
 	requires junit;
 	exports game.tests to junit;
-	exports game.gui;
-	requires java.datatransfer;
-	requires java.desktop;
-    requires javafx.graphics;
-    requires javafx.controls;
 }
