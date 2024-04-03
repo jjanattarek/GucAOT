298bd1269ea3259b605995881b609f76a06ae0e7
mahmoud-hatem-zaky <mahmoud.hatem04@gmail.com>
2024-04-04 01:00:49 +0200

    BuyWeapon Method Added

--- src/game/engine/weapons/factory/WeaponFactory.java
+++ src/game/engine/weapons/factory/WeaponFactory.java
@@ -3,7 +3,10 @@ package game.engine.weapons.factory;
 import java.io.IOException;
 import java.util.HashMap;
 
+import game.engine.Battle;
 import game.engine.dataloader.DataLoader;
+import game.engine.exceptions.InsufficientResourcesException;
+import game.engine.weapons.Weapon;
 import game.engine.weapons.WeaponRegistry;
 
 public class WeaponFactory
@@ -21,5 +24,19 @@ public class WeaponFactory
 		return weaponShop;
 	}
 
+	public FactoryResponse buyWeapon(int resources, int weaponCode) throws InsufficientResourcesException {
+		WeaponRegistry x = weaponShop.get(weaponCode);
+		if(x.getPrice()>resources){
+			throw new InsufficientResourcesException(resources);
+		}
+		else{
+			return new FactoryResponse(x.buildWeapon(),x.getPrice()-resources);
+		}
+		  // I have never felt this smart in my life :D, this took like 40 minutes to find out how to do it without hardcoding it.
+		  //so technically I first get x which is a WeaponRegistry I got from the hash table using the code.
+		  // then I use that registry to check if its price would be higher than the available resources.
+		  // if its fine return a factory response that takes a weapon that I made from the method BuildWeapon.
+		  // and also take the remaining resources
+	}
 }
 //
